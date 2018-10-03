f9af7aab2f2360c95ccf09143a08ee91cb9dd827
Kamil Kwiatkowski <KamilKwiatkowski@Kamils-MacBook-Pro.local>
2018-10-03 17:57:38 +0200

    setting modules

---
+++ app.js
@@ -0,0 +1,22 @@
+// 	BUDGET CONTROLLER
+var budgetController = (function(){
+
+//some code
+
+})();
+
+// IU CONTROLLER
+var UIController = (function(){
+
+
+		//some code
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+
+
+
+})(budgetController, UIController);
