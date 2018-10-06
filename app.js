718c3312ca0eda461aa819f2d269b7d17af6ee24
Kamil Kwiatkowski <KamilKwiatkowski@Kamils-MacBook-Pro.local>
2018-10-06 15:47:43 +0200

    updating the Budget and console.log it

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function(){
 		this.value = value;
 	};
 
+	var calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(cur){
+				sum = sum + cur.value;
+		});
+		data.totals[type] = sum;
+	};
+
 
 	// DATA STRUCTURE !!!
 
@@ -24,7 +32,9 @@ var budgetController = (function(){
 		totals: {
 			exp: 0,
 			inc: 0,
-		}
+		},
+		budget: 0,
+		percentage: -1
 		
 	};
 
@@ -53,6 +63,33 @@ var budgetController = (function(){
 			// return the new element
 			return newItem;
 
+		},
+
+		calculateBudget: function() {
+
+			// calculate total income and expenses 
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			// calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			// calculate the percentage of income thet we spent 
+			if(data.totals.inc > 0){
+				data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)  ;
+			} else {
+				data.percentage = -1;
+			}
+			
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			};
 		},
 
 		testing: function(){
@@ -140,11 +177,16 @@ var controller = (function(budgetCtrl, UICtrl){
 				});
 	};
 		var updateBudget = function() {
+
 				// 1. calculate the budget
+				budgetCtrl.calculateBudget();
 
 				// 2. return the budget
+				var budget = budgetCtrl.getBudget();
 
 				// 3. Display the budget on the UI
+				console.log(budget);
+
 		}
 
 	
