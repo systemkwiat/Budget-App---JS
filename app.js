aa21ba2a580273a530a4c484afa2bc53140e2b76
Kamil Kwiatkowski <KamilKwiatkowski@Kamils-MacBook-Pro.local>
2018-10-08 22:26:13 +0200

    formatting budget numbers: string manipulation

--- app.js
+++ app.js
@@ -164,6 +164,32 @@ var UIController = (function(){
 		container: '.container',
 		expensesPercLabel: '.item__percentage'
 	};
+	var formatNumber =  function(num, type) {
+				var numSplit, int, dec;
+				/*
+				+ or - before number
+				exactly 2 decimal points
+				comma separating the thousands
+				*/
+
+				num = Math.abs(num);
+				num = num.toFixed(2);
+
+				numSplit = num.split('.');
+
+				int = numSplit[0];
+
+				if(int.length > 3){
+					int = int.substr(0, int.length - 3) + ',' + int.substr(int.length-3,int.length);
+				}
+
+				dec = numSplit[1];
+
+				
+
+				return (type === 'exp' ?  '-' : '+') + ' ' + int + '.' + dec;
+
+			};
 
 		return {
 			getInput: function(){
@@ -187,7 +213,7 @@ var UIController = (function(){
 				//replace the placeholder text with some actual data
 				newHtml = html.replace('%id%', obj.id);
 				newHtml = newHtml.replace('%description%', obj.description);
-				newHtml = newHtml.replace('%value%', obj.value);
+				newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 				//insert the html into the DOM
 				document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
@@ -213,10 +239,13 @@ var UIController = (function(){
 			},
 
 			displayBudget: function(obj) {
+				var type;
 
-				document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-				document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-				document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+				obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+				document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+				document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+				document.querySelector(DOMstrings.expenseLabel).textContent = formatNumber(obj.totalExp, 'exp');
 				
 
 				if(obj.percentage > 0) {
