a5158f33c0c2eff0e9623bd01140bee70c156750
Kamil Kwiatkowski <KamilKwiatkowski@Kamils-MacBook-Pro.local>
2018-10-08 18:57:25 +0200

    updating the percentages: Budget Controller

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var budgetController = (function(){
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
+	};
+
+	Expense.prototype.calcPercentage = function(totalIncome){
+		if(totalIncome > 0) {
+			this.percentage = Math.round((this.value / totalIncome) * 100);
+		} else {
+			this.percentage = -1;
+		}
+	};
+
+	Expense.prototype.getPercentage = function() {
+		return this.percentage;
 	};
 
 	var Income = function(id, description, value){
@@ -95,6 +108,24 @@ var budgetController = (function(){
 				data.percentage = -1;
 			}
 			
+		},
+
+		calculatePercentages: function() {
+
+
+			data.allItems.exp.forEach(function(cur){
+				cur.calcPercentage(data.totals.inc);
+			});
+
+
+
+		},
+
+		getPercentages: function(){
+			var allPerc = data.allItems.exp.map(function(cur){
+				return cur.getPercentage();
+			});
+			return allPerc;
 		},
 
 		getBudget: function() {
@@ -231,7 +262,17 @@ var controller = (function(budgetCtrl, UICtrl){
 				// 3. Display the budget on the UI
 				UICtrl.displayBudget(budget);
 
-		}
+		};
+
+		var updatePercentages = function() {
+
+			// 1. Calculate percentages 
+			budgetCtrl.calculatePercentages();
+			// 2. Reade percentages from the budget controller
+			var percentages = budgetCtrl.getPercentages();
+			// 3. Update the UI with the new percentage
+			console.log(percentages)
+		};
 
 	
 		var ctrlAddItem = function(){
@@ -252,6 +293,9 @@ var controller = (function(budgetCtrl, UICtrl){
 
 		// 5. Calculate and update budget
 		updateBudget();
+
+		// 6. Calculate and update the percetages
+		updatePercentages();
 		}
 
 		
@@ -275,6 +319,8 @@ var controller = (function(budgetCtrl, UICtrl){
 				UICtrl.deleteListItem(itemID);
 				// 3. update and show the new budget
 				updateBudget();
+				// 4. Calculate and update the percetages
+				updatePercentages();
 
 			}
 		};
